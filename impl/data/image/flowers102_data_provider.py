08a3bcbc2b104db537010a3e571e3f6feed8db32
meierbe8 <meierbe8@students.zhaw.ch>
2017-10-17 15:35:41 +0200

    Implemented the flowers102 dataset

---
+++ impl/data/image/flowers102_data_provider.py
@@ -0,0 +1,47 @@
+import numpy as np
+from random import Random
+
+from impl.data.misc import flowers102
+
+from impl.data.image.image_data_provider import ImageDataProvider
+
+
+class Flowers102DataProvider(ImageDataProvider):
+    def __init__(self, train_classes=None, validate_classes=None, test_classes=None,
+                 min_cluster_count=None, max_cluster_count=None, target_img_size=(48, 48)):
+        self._target_img_size = target_img_size
+        if train_classes is None and validate_classes is None and test_classes is None:
+            rand = Random()
+            rand.seed(1337)
+            classes = list(range(102))
+            rand.shuffle(classes)
+            train_classes_count = int(0.8 * len(classes))
+            train_classes = classes[:train_classes_count]
+            validate_classes = classes[train_classes_count:]
+            test_classes = classes[train_classes_count:]
+        super().__init__(train_classes, validate_classes, test_classes, min_cluster_count, max_cluster_count, center_data=True)
+
+    def _get_img_data_shape(self):
+        return self._target_img_size + (3,)
+
+    def _load_data(self):
+
+        # Load all records
+        (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = flowers102.load_data(self._target_img_size)
+
+        # Merge them (we split them by classes)
+        x = np.concatenate((x_train, x_valid, x_test))
+        y = np.concatenate((y_train, y_valid, y_test))
+
+        # Reshape x for tensorflow
+        x = x.reshape((x.shape[0],) + self.get_data_shape())
+
+        # Normalize x to [0, 1]
+        x = self._scale_data(x)
+
+        # Split the records by classes and store it
+        return {i: x[y == i] for i in np.unique(y)}
+
+if __name__ == '__main__':
+    dp = Flowers102DataProvider()
+
---
+++ impl/data/misc/flowers102.py
@@ -0,0 +1,90 @@
+import gzip
+import os
+import tarfile
+import glob
+import pickle
+
+import shutil
+
+from scipy.io import loadmat
+from scipy.misc import imread, imresize
+
+from keras.utils.data_utils import get_file
+import numpy as np
+
+def load_data(target_img_size=(48, 48)):
+    """Loads the Flowers102 dataset
+    # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_valid, y_valid), (x_test, y_test)`.
+    """
+    dirname = os.path.join('datasets', 'flowers102')
+    # http://www.robots.ox.ac.uk/~vgg/data/flowers/102/102flowers.tgz
+    # http://www.robots.ox.ac.uk/~vgg/data/flowers/102/imagelabels.mat
+    base = 'http://www.robots.ox.ac.uk/~vgg/data/flowers/102/'
+    files = ['102flowers.tgz', 'imagelabels.mat', 'setid.mat']
+
+    # Download all files
+    paths = []
+    for file in files:
+        paths.append(get_file(file, origin=base + file, cache_subdir=dirname))
+
+    # Load all files
+    assert len(target_img_size) == 2
+    cached_file = os.path.expanduser(os.path.join('~', '.keras', dirname, 'img_{}x{}.cache.pkl'.format(*target_img_size)))
+
+    # If required: Pre-process all files
+    if not os.path.exists(cached_file):
+        # The following code is partially based on:
+        # https://github.com/Arsey/keras-transfer-learning-for-oxford102/blob/master/bootstrap.py
+
+        tmp_dir = os.path.expanduser(os.path.join('~', '.keras', dirname, 'tmp'))
+        if not os.path.exists(tmp_dir):
+            os.makedirs(tmp_dir)
+
+        print("Extract all flowers images...")
+        tarfile.open(paths[0]).extractall(tmp_dir)
+
+        print("Load index definitions...")
+        idx = loadmat(paths[2])
+        idx_train = idx['trnid'][0] - 1
+        idx_test = idx['tstid'][0] - 1
+        idx_valid = idx['valid'][0] - 1
+
+        print("Load labels...")
+        img_labels = loadmat(paths[1])['labels'][0] - 1
+
+        print("Load and pre-process image files...")
+        img_files = sorted(glob.glob(os.path.join(tmp_dir, 'jpg', '*.jpg')))
+        x = np.zeros((len(img_files),) + target_img_size + (3,), dtype=np.uint8)
+        y = np.zeros((len(img_files)))
+        for i in range(len(img_files)):
+            img = imread(img_files[i])
+            img = imresize(img, target_img_size + (3,))
+            x[i] = img
+            y[i] = img_labels[i]
+
+        print("Split the data...")
+        x_train = x[idx_train]
+        y_train = y[idx_train]
+        x_valid = x[idx_valid]
+        y_valid = y[idx_valid]
+        x_test = x[idx_test]
+        y_test = y[idx_test]
+
+        print("Delete temporary files")
+        shutil.rmtree(tmp_dir)
+
+        print("Save the data")
+        data = ((x_train, y_train), (x_valid, y_valid), (x_test, y_test))
+        with open(cached_file, "wb") as fh:
+            pickle.dump(data, fh)
+
+    with open(cached_file, "rb") as fh:
+        (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = pickle.load(fh)
+
+    return (x_train, y_train), (x_valid, y_valid), (x_test, y_test)
+
+
+if __name__ == '__main__':
+    load_data()
+
